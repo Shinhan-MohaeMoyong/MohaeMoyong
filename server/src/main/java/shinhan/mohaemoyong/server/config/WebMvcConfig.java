c309018dcc19bbb360181193664b254b9b41fd46
송성현 <161740297+SHSong99@users.noreply.github.com>
2025-08-15 16:22:02 +0900

    feature : 서버 최초 설정 (#2)

---
+++ server/src/main/java/shinhan/mohaemoyong/server/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package shinhan.mohaemoyong.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
+    // 우선 모든 REST 메서드에 허용
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
