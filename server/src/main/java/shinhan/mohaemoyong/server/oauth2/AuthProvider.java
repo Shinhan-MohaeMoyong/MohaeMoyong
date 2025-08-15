13727374afa5609733378285259d94f839dcadbb
SHSong <99pyues2@gmail.com>
2025-08-15 16:57:50 +0900

    feature : 디렉토리 구조 설정 및 소셜로그인 구현

---
+++ server/src/main/java/shinhan/mohaemoyong/server/oauth2/AuthProvider.java
@@ -0,0 +1,8 @@
+package shinhan.mohaemoyong.server.oauth2;
+
+public enum AuthProvider {
+   // 우선 카카오만 소셜
+    kakao,
+    local
+
+}
---
+++ server/src/main/java/shinhan/mohaemoyong/server/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package shinhan.mohaemoyong.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import shinhan.mohaemoyong.server.model.entity.User;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
