406c67f5130ca0fdeb8a136716c22f2ea50732d0
송성현 <161740297+SHSong99@users.noreply.github.com>
2025-08-20 10:43:15 +0900

    release : health API (#46)

---
+++ server/src/main/java/shinhan/mohaemoyong/server/controller/HealthController.java
@@ -0,0 +1,15 @@
+package shinhan.mohaemoyong.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// https 상태검사 로드밸런싱 health API
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
