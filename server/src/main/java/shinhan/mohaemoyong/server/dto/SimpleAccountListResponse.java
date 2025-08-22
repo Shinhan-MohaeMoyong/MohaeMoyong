716a2e4ccf55fa0188314a71a7b8ce3e6d8513c9
송성현 <161740297+SHSong99@users.noreply.github.com>
2025-08-22 23:27:56 +0900

    feature : 계좌 목록 조회 (#75)

---
+++ server/src/main/java/shinhan/mohaemoyong/server/dto/SimpleAccountListResponse.java
@@ -0,0 +1,22 @@
+package shinhan.mohaemoyong.server.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import shinhan.mohaemoyong.server.adapter.deposit.dto.response.InquireDemandDepositAccountListResponse;
+
+@Getter
+@Builder
+public class SimpleAccountListResponse {
+    private String accountNo;
+    private Long accountBalance;
+    private String accountName;
+
+    public static SimpleAccountListResponse toDto(InquireDemandDepositAccountListResponse.Record record) {
+        return SimpleAccountListResponse.builder()
+                .accountNo(record.getAccountNo())
+                .accountBalance(record.getAccountBalance())
+                .accountName(record.getAccountName())
+                .build();
+    }
+
+}
