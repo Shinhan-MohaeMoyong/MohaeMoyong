bfa08679de1f2bf1fad672026f2161731f21a7ff
SHSong <99pyues2@gmail.com>
2025-08-29 10:10:26 +0900

    feature : 계좌목록 조회에 1원송금 인증여부 응답 추가

--- server/src/main/java/shinhan/mohaemoyong/server/dto/SimpleAccountListResponse.java
+++ server/src/main/java/shinhan/mohaemoyong/server/dto/SimpleAccountListResponse.java
@@ -11,16 +11,21 @@ public class SimpleAccountListResponse {
     private String accountNo;
     private Long accountBalance;
     private String accountName;
+    private Boolean authenticated;
 
+    // 우리 DB 에서 필요한 데이터는 ourAccount, 금융망 DB 에서 필요한 데이터는 record
     public static SimpleAccountListResponse toDto(InquireDemandDepositAccountListResponse.Record record, Accounts ourAccount) {
-        // 우리 DB에 저장된 계좌 별칭(accountName)이 있다면 그것을 사용하고,
-        // 없다면(null이면) 어댑터에서 받은 계좌명을 예비로 사용
+        // 우리 DB에 저장된 계좌 별칭(accountName)이 있다면 그것을 사용하고, 없다면(null이면) 어댑터에서 받은 계좌명을 예비로 사용
         String finalAccountName = (ourAccount != null) ? ourAccount.getAccountName() : record.getAccountName();
+
+        // 혹시나 우리 DB 에 등록이 되어있지 않은 경우 NullPointerException 방지
+        Boolean finalAuthenticated = (ourAccount != null) ? ourAccount.getAuthenticated() : false;
 
         return SimpleAccountListResponse.builder()
                 .accountNo(record.getAccountNo())
                 .accountBalance(record.getAccountBalance())
                 .accountName(finalAccountName)
+                .authenticated(finalAuthenticated)
                 .build();
     }
 
