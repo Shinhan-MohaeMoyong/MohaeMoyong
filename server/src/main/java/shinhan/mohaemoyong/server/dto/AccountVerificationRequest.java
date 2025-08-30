ada4f84ca00b1ea02bf6287586535855b2a1ff22
SHSong <99pyues2@gmail.com>
2025-08-30 10:44:36 +0900

    fix : dto에 생성자

--- server/src/main/java/shinhan/mohaemoyong/server/dto/AccountVerificationRequest.java
+++ server/src/main/java/shinhan/mohaemoyong/server/dto/AccountVerificationRequest.java
@@ -1,10 +1,12 @@
 package shinhan.mohaemoyong.server.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AccountVerificationRequest {
     private String accountNo;
     private String authCode;
