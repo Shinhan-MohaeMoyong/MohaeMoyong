4525289a3a82d5495455e4f3e3d046b511596b76
송성현 <161740297+SHSong99@users.noreply.github.com>
2025-08-30 09:13:28 +0900

    fix : Update WebMvcConfig.java

--- server/src/main/java/shinhan/mohaemoyong/server/config/WebMvcConfig.java
+++ server/src/main/java/shinhan/mohaemoyong/server/config/WebMvcConfig.java
@@ -18,6 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
